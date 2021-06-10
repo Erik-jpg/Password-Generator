025ea5109b9adec006441215c200b829deede12b
Erik Stone <spologas@gmail.com>
2021-06-09 23:54:39 -0700

    tried making Generate Password button clicked for prompt but error saying it isn't that kind of object

--- script.js
+++ script.js
@@ -5,20 +5,19 @@ let passwordLength = parseInt(
   );
   while (passwordLength < 8 || passwordLength > 128 || !passwordLength) {
     alert("Please enter correct password length");
-    passwordLength = parseInt(
-      prompt("How long would you like your password to be?")
+    passwordLength = parseInt( 
     );
   }
-  let specialChars = confirm("Would you like to use special characters, (!@#$)?");
-  let uppercaseChars = confirm("Would you like to use capitalized letters, (ABCD)?");
-  let lowercaseChars = confirm("Would you like lowercase letters, (abcd)?");
-  let numberChars = confirm("Would you like to include numbers, (1234)?");
-  while (!specialChars && !uppercaseChars && !lowercaseChars && !numberChars) {
+  let specialCharacters = confirm("Would you like to use special characters, such as: (.,-=!@#$)?");
+  let capitalizedCharacters = confirm("Would you like to use capitalized letters, such as: (ABCDEFGH)?");
+  let lowercaseCharacters = confirm("Would you like lowercase letters, such as: (abcdefgh)?");
+  let numericCharacters = confirm("Would you like to include numbers, such as: (12345678)?");
+  while (!specialCharacters && !capitalizedCharacters && !lowercaseCharacters && !numericCharacters) {
     alert("You need to chose at least one.");
-    specialChars = confirm("Would you like special characters(!@#$)?");
-    uppercaseChars = confirm("Would you like uppercase  characters(ABCD)?");
-    lowercaseChars = confirm("Would you like lowercase characters(abcd)?");
-    numberChars = confirm("Would you like to include numbers(1234)?");
+    specialCharacters = confirm("Would you like special characters(!@#$)?");
+    capitalizedCharacters = confirm("Would you like uppercase  characters(ABCD)?");
+    lowercaseCharacters = confirm("Would you like lowercase characters(abcd)?");
+    numericCharacters = confirm("Would you like to include numbers(1234)?");
   }
   const upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   const lowerCaseArray = ['a', 'b', 'c', 'd', 'e','f', 'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'];
@@ -29,26 +28,26 @@ let passwordLength = parseInt(
     for (let index = 0; index < maxNum; index++) {
       let rand = Math.floor(Math.random() * 4);
       while (
-        (!uppercaseChars && rand === 0) ||
-        (!lowercaseChars && rand === 1) ||
-        (!specialChars && rand === 2) ||
-        (!numberChars && rand === 3)
+        (!capitalizedCharacters && rand === 0) ||
+        (!lowercaseCharacters && rand === 1) ||
+        (!specialCharacters && rand === 2) ||
+        (!numericCharacters && rand === 3)
         ){
           rand = Math.floor(Math.random() * 4); 
       }
-      if (uppercaseChars && rand === 0) {
+      if (capitalizedCharacters && rand === 0) {
           let upperCaseRand = Math.floor(Math.random() * upperCaseArray.length);
           array.push(upperCaseArray[upperCaseRand]);
         }
-      if (lowercaseChars && rand === 1) {
+      if (lowercaseCharacters && rand === 1) {
         let lowerCaseRand = Math.floor(Math.random() * lowerCaseArray.length);
         array.push(lowerCaseArray[lowerCaseRand]);
       }
-      if (specialChars && rand === 2) {
+      if (specialCharacters && rand === 2) {
         let specialRand = Math.floor(Math.random() * specialArray.length);
         array.push(specialArray[specialRand]);
       }
-      if (numberChars && rand === 3) {
+      if (numericCharacters && rand === 3) {
         let numbersRand = Math.floor(Math.random() * numbersArray.length);
         array.push(numbersArray[numbersRand]);
       }
